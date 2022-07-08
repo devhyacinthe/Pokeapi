b36f095f63fea67596b5cc4bd53ca6c7ca044975
devhyacinthe <yacinthedalmeida2002@gmail.com>
2022-07-08 20:21:58 +0000

    Mise en place du style

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
+let typePokemons = []
+let idPokemons = []
+let imagePokemons = []
+let taillePokemons = []
+let poidsPokemons = []
 
 const getAllPokemons = async () => {
-
     //for search all pokemons on api
     await fetch('https://pokeapi.co/api/v2/pokemon')
             .then((res) => res.json())
@@ -10,12 +14,23 @@ const getAllPokemons = async () => {
                     fetch(`https://pokeapi.co/api/v2/pokemon/${element.name}`)
                     .then((res) => res.json())
                     .then((data) => {
-                        console.log(data.name);
+                       typePokemons.push(data.types[0].type.name)
+                       idPokemons.push(data.id)
+                       imagePokemons.push(data.sprites.back_default)
+                       taillePokemons.push(data.height)
+                       poidsPokemons.push(data.weight)
+                       console.log(data);
                     })
                 });
             })
-
-    //for see details of all pokemons
+            
+    console.log(typePokemons);
+    console.log(idPokemons);
+    console.log(imagePokemons);
+    console.log(taillePokemons);
+    console.log(poidsPokemons);
+    
    
 }
-getAllPokemons()  
+getAllPokemons()
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+# PokeApi
+
+### Il s'agit d'un exercice classique d'utilisation d'api
+
+## Information sur le pokemon
+- type
+- id 
+- nom
+- image
+- taille
+- poids
+
+# differents types
+- grass (herbe) -> vert
+- fire (feu) -> orange
+- water (eau) -> bleu
+- bug (insecte) -> marron
+- normal (normal) -> blanc
